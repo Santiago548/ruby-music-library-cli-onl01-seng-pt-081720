e49828b087b50393344e319154644b982682bb32
Dominic Barajas <dominicbarajas548@gmail.com>
2020-09-27 18:28:39 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -7,9 +7,9 @@ class MusicLibraryController
   end
 
   def call
+  input = ""
 
-    input = ""
-
+  while input != "exit"
     puts "Welcome to your music library!"
     puts "To list all of your songs, enter 'list songs'."
     puts "To list all of the artists in your library, enter 'list artists'."
@@ -22,29 +22,28 @@ class MusicLibraryController
 
     input = gets.strip
 
+    case input
+    when "list songs"
+      list_songs
+    when "list artists"
+      list_artists
+    when "list genres"
+      list_genres
+    when "list artist"
+      list_songs_by_artist
+    when "list genre"
+      list_songs_by_genre
+    when "play song"
+      play_song
+    end
   end
+end
 
-  def list_songs
-
+def list_songs
+  Song.all.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |s, i|
+    puts "#{i}. #{s.artist.name} - #{s.name} - #{s.genre.name}"
   end
+end
 
-  def list_artists
 
-  end
-
-  def list_genres
-
-  end
-
-  def list_songs_by_artist
-
-  end
-
-  def list_songs_by_genre
-
-  end
-
-  def play_song
-
-  end
 end
